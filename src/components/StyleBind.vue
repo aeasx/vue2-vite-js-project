f30114a69cd227b9d87d00d5496914c22b867379
aeasx <3089326386@qq.com>
2025-11-16 21:06:07 +0800

    chore: 绑定内联样式

--- src/components/StyleBind.vue
+++ src/components/StyleBind.vue
@@ -5,11 +5,34 @@ export default {
     return {
       isActive: false
     }
+  },
+  methods: {
+    handleToggle() {
+      return this.isActive = !this.isActive
+    }
   }
 }
 </script>
 <template>
   <div>
-    <el-button type="primary">我是element按钮</el-button>
+    <el-button type="primary" @click="handleToggle">Toggle</el-button>
+    <div class="box" :style="{ backgroundColor: isActive ? 'green' : 'red' }">
+      我是Box
+    </div>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.box {
+  width: 100px;
+  height: 100px;
+}
+
+.active {
+  background-color: rebeccapurple;
+}
+
+.danger {
+  background-color: red;
+}
+</style>
